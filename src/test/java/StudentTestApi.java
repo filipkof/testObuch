7456fed7e0aa37b05bbc260eba7db36937e0cde4
filipkov <w1q2w3@yandex.ru>
2025-12-16 00:56:55 +0300

    Провел небольшой рефактор тестов, которые проверяют выполнение прописанных для него условий, а так же прогнал на сломанном микросервисе и получил ряд ошибок

--- src/test/java/StudentTestApi.java
+++ src/test/java/StudentTestApi.java
@@ -1,49 +1,39 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.example.StudentGenerator;
 import org.example.WireMock;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class StudentTestApi {
 
-    WireMock wireMock = new WireMock(5352);
-    String baseUrl = "http://localhost";
+    StudentGenerator studentGenerator = new StudentGenerator();
 
     @BeforeAll
     static void settings() {
         RestAssured.baseURI = "http://localhost";
-        RestAssured.port = 5352;
-    }
-
-    @BeforeEach
-    void setUp() {
-        wireMock.startWireMockServer();
-    }
-
-    @AfterEach
-    void tearDown() {
-        wireMock.stopWireMockServer();
+        RestAssured.port = 8080;
     }
 
     @Test
     void testGetStudentValidId() {
+        studentGenerator.createStudent("1", "Андрей");
         given()
                 .when().get("/student/1")
                 .then()
                 .statusCode(200)
                 .body("name", equalTo("Андрей"),
-                        "id", equalTo("1"));
+                        "id", equalTo(1));
     }
 
     @Test
     void testGetStudentInvalidId() {
         given()
-                .when().get("/student/2")
+                .when().get("/student/9999")
                 .then()
                 .statusCode(404);
     }
@@ -61,21 +51,27 @@ public class StudentTestApi {
                 .when().post("/student")
                 .then()
                 .statusCode(201);
+        String responseBody = studentGenerator.getStudent(777);
+        Assertions.assertFalse(responseBody.isBlank());
     }
 
     @Test
     void testUpdateStudentOldIdNewValidName() {
+        studentGenerator.createStudent("555", "Андрей");
         given()
                 .contentType("application/json")
                 .body("""
                         {
-                          "id": "777",
+                          "id": "555",
                           "name": "Дмитрий"
                         }
                         """)
                 .when().post("/student")
                 .then()
                 .statusCode(201);
+        String responseBody = studentGenerator.getStudent(555);
+        String name = JsonPath.from(responseBody).getString("name");
+        Assertions.assertEquals("Дмитрий", name);
     }
 
     @Test
@@ -90,7 +86,7 @@ public class StudentTestApi {
                 .when().post("/student")
                 .then()
                 .statusCode(201)
-                .body("id", notNullValue());
+                .body(not(isEmptyOrNullString()));
     }
 
     @Test
@@ -108,21 +104,25 @@ public class StudentTestApi {
 
     @Test
     void testDeleteStudentValidId() {
+        studentGenerator.createStudent("10", "Андрей");
         given()
-                .when().delete("/student/2")
+                .when().delete("/student/10")
                 .then()
                 .statusCode(200);
+        String responseBody = studentGenerator.getStudent(10);
+        Assertions.assertTrue(responseBody.isBlank());
     }
 
     @Test
     void testDeleteStudentInvalidId() {
         given()
-                .when().delete("/student/3")
+                .when().delete("/student/2222")
                 .then()
                 .statusCode(404);
     }
 
     @Test
+    @Order(1)
     void testGetTopStudentsEmptyList() {
         String body = given()
                 .when().get("/topStudent")
@@ -133,7 +133,9 @@ public class StudentTestApi {
     }
 
     @Test
+    @Order(2)
     void testGetTopStudentsWithoutGrades() {
+        studentGenerator.createStudent("272", "Михаил");
         String body = given()
                 .when().get("/topStudent")
                 .then()
@@ -143,24 +145,30 @@ public class StudentTestApi {
     }
 
     @Test
+    @Order(3)
     void testGetTopStudentWithMaxAverageGrade() {
+        studentGenerator.createStudentWithMarks("828", "Олег", new int[]{5, 5, 5, 5});
         given()
                 .when().get("/topStudent")
                 .then()
                 .statusCode(200)
-                .body("id", notNullValue(),
-                        "name", notNullValue());
+                .body("size()", equalTo(1),
+                        "[0].id", equalTo(828),
+                        "[0].name", equalTo("Олег"));
     }
 
     @Test
+    @Order(4)
     void testGetTopStudentsWithMaxAverageGrades() {
+        studentGenerator.createStudentWithMarks("827", "Гело", new int[]{5, 5, 5, 5});
         given()
                 .when().get("/topStudent")
                 .then()
                 .statusCode(200)
-                .body("students", not(empty()))
-                .body("students[1].id", notNullValue())
-                .body("students[1].name", notNullValue());
+                .body("size()", equalTo(2),
+                        "[0].id", equalTo(828),
+                        "[0].name", equalTo("Олег"),
+                        "[1].id", equalTo(827),
+                        "[1].name", equalTo("Гело"));
     }
-
 }
